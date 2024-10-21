4a1668652fddfeb34e1670017e16a65e096d881b
Geonhwi Park <ghpark715@gmail.com>
2024-10-21 11:39:38 +0900

    Spring Boot Init

---
+++ DearPet/src/main/java/com/dearpet/dearpet/DearPetApplication.java
@@ -0,0 +1,13 @@
+package com.dearpet.dearpet;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DearPetApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DearPetApplication.class, args);
+	}
+
+}
