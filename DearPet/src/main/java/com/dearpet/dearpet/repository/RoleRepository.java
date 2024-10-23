aab9011b6943e10c5b1c7c9ca1b06bc1a037dc6c
Geonhwi Park <geonhwipark715@gmail.com>
2024-10-23 14:20:03 +0900

    Feat: Role Repository

---
+++ DearPet/src/main/java/com/dearpet/dearpet/repository/RoleRepository.java
@@ -0,0 +1,10 @@
+package com.dearpet.dearpet.repository;
+
+import com.dearpet.dearpet.entity.Role;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.Optional;
+
+public interface RoleRepository extends JpaRepository<Role, Long> {
+    Optional<Role> findByRoleName(String roleName);
+}
