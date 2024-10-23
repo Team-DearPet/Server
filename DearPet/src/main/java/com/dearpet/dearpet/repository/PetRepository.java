70764c638f820d75057bdf18578028d718c38138
wijihooooon <dnlwlgns1117@naver.com>
2024-10-24 01:35:59 +0900

    Update: PetRepository-수정
    findByPetId() 제거

--- DearPet/src/main/java/com/dearpet/dearpet/repository/PetRepository.java
+++ DearPet/src/main/java/com/dearpet/dearpet/repository/PetRepository.java
@@ -4,13 +4,15 @@ import com.dearpet.dearpet.entity.Pet;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import java.util.List;
-import java.util.Optional;
 
+/*
+ * Pet Repository
+ * @Author 위지훈
+ * @Since 2024.10.23
+ */
 public interface PetRepository extends JpaRepository<Pet, Long> {
 
     // userId로 사용자가 보유한 펫 목록 조회
     List<Pet> findByUserUserId(Long userId);
 
-    // petId로 펫 세부 정보 조회
-    Optional<Pet> findByPetId(Long petId);
 }
