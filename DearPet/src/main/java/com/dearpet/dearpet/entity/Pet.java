9c58afeaebe59cd0740b645ac88f44307a31c04a
wijihooooon <dnlwlgns1117@naver.com>
2024-10-23 10:15:18 +0900

    Feat: Pets Entitiy 추가

---
+++ DearPet/src/main/java/com/dearpet/dearpet/entity/Pet.java
@@ -0,0 +1,38 @@
+package com.dearpet.dearpet.entity;
+
+import jakarta.persistence.*;
+import lombok.*;
+
+import java.math.BigDecimal;
+
+@Entity
+@Getter
+@Setter
+@Table(name = "Pets")
+public class Pet {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long petId;                 // PK
+    private String petName;             // 펫 이름
+    private String species;             // 펫 종류
+    private int age;                    // 펫 나이
+    private boolean neutered = false;   // 펫 중성화 여부
+
+    @Enumerated(EnumType.STRING)
+    private Gender gender;              // 펫 성별
+
+    @Column(precision = 5, scale = 2)
+    private BigDecimal weight;          // 펫 몸무게
+
+    @Lob
+    private String healthStatus;        // 펫 건강정보
+
+    @ManyToOne
+    @JoinColumn(name = "user_id")
+    private User user;
+
+    public enum Gender{
+        MALE, FEMALE
+    }
+}
