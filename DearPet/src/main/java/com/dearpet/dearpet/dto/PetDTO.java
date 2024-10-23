c0c7e5bb9e56cd77dd51419a6d0a672a3625d17b
wijihooooon <dnlwlgns1117@naver.com>
2024-10-24 01:35:05 +0900

    Update: PetDTO-수정
    Gender Enum 타입 오류 해결
    Getter/Setter loombok으로 변경
    클래스 주석 추가

--- DearPet/src/main/java/com/dearpet/dearpet/dto/PetDTO.java
+++ DearPet/src/main/java/com/dearpet/dearpet/dto/PetDTO.java
@@ -1,22 +1,36 @@
 package com.dearpet.dearpet.dto;
 
+import com.dearpet.dearpet.entity.Pet;
+import lombok.Getter;
+import lombok.Setter;
+import org.springframework.security.core.parameters.P;
+
 import java.math.BigDecimal;
 
+/*
+ * Pet DTD
+ * @Author 위지훈
+ * @Since 2024.10.23
+ */
+@Getter
+@Setter
 public class PetDTO {
     private Long petId;                 // PK
     private String name;                // 펫 이름
     private String species;             // 펫 종류
     private int age;                    // 펫 나이
-    private boolean neutered;   // 펫 중성화 여부
-    private String gender;              // 펫 성별
+    private boolean neutered;           // 펫 중성화 여부
+    private Pet.Gender gender;          // 펫 성별
     private BigDecimal weight;          // 펫 몸무게
     private String healthStatus;        // 펫 건강상태
+    private Long userId;                // 사용자 ID
 
     // 기본 생성자
-    public PetDTO() {}
+    public PetDTO() {
+    }
 
     // 생성자
-    public PetDTO(Long petId, String name, String species, int age, boolean neutered, String gender, BigDecimal weight, String healthStatus) {
+    public PetDTO(Long petId, String name, String species, int age, boolean neutered, Pet.Gender gender, BigDecimal weight, String healthStatus, Long userId) {
         this.petId = petId;
         this.name = name;
         this.species = species;
@@ -25,70 +39,7 @@ public class PetDTO {
         this.gender = gender;
         this.weight = weight;
         this.healthStatus = healthStatus;
+        this.userId = userId;
     }
 
-    // Getter/Setter
-    public Long getPetId() {
-        return petId;
-    }
-
-    public void setPetId(Long petId) {
-        this.petId = petId;
-    }
-
-    public String getName() {
-        return name;
-    }
-
-    public void setName(String name) {
-        this.name = name;
-    }
-
-    public String getSpecies() {
-        return species;
-    }
-
-    public void setSpecies(String species) {
-        this.species = species;
-    }
-
-    public int getAge() {
-        return age;
-    }
-
-    public void setAge(int age) {
-        this.age = age;
-    }
-
-    public boolean isNeutered() {
-        return neutered;
-    }
-
-    public void setNeutered(boolean neutered) {
-        this.neutered = neutered;
-    }
-
-    public String getGender() {
-        return gender;
-    }
-
-    public void setGender(String gender) {
-        this.gender = gender;
-    }
-
-    public BigDecimal getWeight() {
-        return weight;
-    }
-
-    public void setWeight(BigDecimal weight) {
-        this.weight = weight;
-    }
-
-    public String getHealthStatus() {
-        return healthStatus;
-    }
-
-    public void setHealthStatus(String healthStatus) {
-        this.healthStatus = healthStatus;
-    }
 }
