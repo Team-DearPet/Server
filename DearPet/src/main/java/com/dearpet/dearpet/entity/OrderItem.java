124ec87551c06a09746e855a4277100374c9424e
wijihooooon <dnlwlgns1117@naver.com>
2024-10-24 02:55:28 +0900

    Feat: OrderItem Entity - 작성

---
+++ DearPet/src/main/java/com/dearpet/dearpet/entity/OrderItem.java
@@ -0,0 +1,34 @@
+package com.dearpet.dearpet.entity;
+
+import jakarta.persistence.*;
+import lombok.Getter;
+import lombok.Setter;
+
+import java.math.BigDecimal;
+
+@Entity
+@Getter
+@Setter
+@Table(name = "OrderItems")
+public class OrderItem {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private int orderItemId;        // PK
+    private int quantity;           // 수량
+
+    @Column(precision = 10, scale = 2)
+    private BigDecimal price;       // 가격
+
+    @ManyToOne
+    @JoinColumn(name = "order_id")
+    private Order order;            // 주문 상세 : 주문 = N : 1
+
+    /*
+    ProductEntity가 생길때까지 주석처리
+    @ManyToOne
+    @JoinColumn(name = "product_id")
+    private Product product;        // 주문 상세 : 상품 = N : 1
+    */
+     
+}
