7a95ff5825ef135d6b85712be6291432eaeddc6d
wijihooooon <dnlwlgns1117@naver.com>
2024-10-23 13:56:14 +0900

    Feat: Pet Repository
    userId로 사용자 보유 펫 목록 조회
    petId로 펫 세부 정보 조회
    기능 추가

--- DearPet/src/main/java/com/dearpet/dearpet/repository/PetRepository.java
+++ DearPet/src/main/java/com/dearpet/dearpet/repository/PetRepository.java
@@ -3,5 +3,14 @@ package com.dearpet.dearpet.repository;
 import com.dearpet.dearpet.entity.Pet;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.List;
+import java.util.Optional;
+
 public interface PetRepository extends JpaRepository<Pet, Long> {
+
+    // userId로 사용자가 보유한 펫 목록 조회
+    List<Pet> findByUserUserId(Long userId);
+
+    // petId로 펫 세부 정보 조회
+    Optional<Pet> findByPetId(Long petId);
 }
