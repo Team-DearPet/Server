9a073915a0c9232674c551b9d7ec756552358a59
wijihooooon <dnlwlgns1117@naver.com>
2024-10-25 04:25:32 +0900

    Update: Order Controller - 기능 추가

--- DearPet/src/main/java/com/dearpet/dearpet/controller/OrderController.java
+++ DearPet/src/main/java/com/dearpet/dearpet/controller/OrderController.java
@@ -1,6 +1,7 @@
 package com.dearpet.dearpet.controller;
 
 import com.dearpet.dearpet.dto.OrderDTO;
+import com.dearpet.dearpet.dto.OrderItemDTO;
 import com.dearpet.dearpet.service.OrderService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
@@ -19,31 +20,42 @@ public class OrderController {
 
     @Autowired
     private OrderService orderService;
-/* 토큰에서 userId를 뽑아와야함
-    // 사용자 주문 내역 조회
-    @GetMapping
-    public ResponseEntity<List<OrderDTO>> getOrderByUserId(){
 
-        List<OrderDTO> oderList = orderService.getOrderByUserId(userId);
-        return ResponseEntity.ok(orderList);
-    }
-*/
+    /* 토큰에서 userId를 뽑아와야함
+        // 사용자 주문 내역 조회
+        @GetMapping
+        public ResponseEntity<List<OrderDTO>> getOrderByUserId(){
+
+            List<OrderDTO> oderList = orderService.getOrderByUserId(userId);
+            return ResponseEntity.ok(orderList);
+        }
+    */
     // 주문 생성
     @PostMapping
-    public ResponseEntity<OrderDTO> createOrder(@RequestBody OrderDTO orderDTO){
+    public ResponseEntity<OrderDTO> createOrder(@RequestBody OrderDTO orderDTO) {
         OrderDTO createdOrder = orderService.createOrder(orderDTO);
         return ResponseEntity.ok(createdOrder);
     }
 
     // 주문 정보 상세 조회
+    @GetMapping("/orderId")
+    public ResponseEntity<OrderDTO> getOrderById(@RequestParam("orderId") Long orderId) {
+        OrderDTO orderDTO = orderService.getOrderByOrderId(orderId);
+        return ResponseEntity.ok(orderDTO);
+    }
 
     // 주문 취소
     @PatchMapping("/{orderId}")
-    public ResponseEntity<OrderDTO> cancelOrder(@PathVariable Long orderId){
+    public ResponseEntity<OrderDTO> cancelOrder(@PathVariable Long orderId) {
         OrderDTO cancelledOrder = orderService.cancelOrder(orderId);
         return ResponseEntity.ok(cancelledOrder);
-
     }
 
     // 주문한 상품 목록 조회
+    @GetMapping("/{orderId}/items")
+    public ResponseEntity<List<OrderItemDTO>> getOrderItems(@PathVariable Long orderId) {
+        List<OrderItemDTO> orderItemList = orderService.getOrderItemByOrderId(orderId);
+        return ResponseEntity.ok(orderItemList);
+    }
+
 }
