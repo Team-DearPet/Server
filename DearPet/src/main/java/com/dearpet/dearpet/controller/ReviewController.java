60f80cb2e128450fb039778b4ba788d485a021bf
Geonhwi Park <ghpark715@gmail.com>
2024-10-29 14:06:42 +0900

    Update: 경로 설정

--- DearPet/src/main/java/com/dearpet/dearpet/controller/ReviewController.java
+++ DearPet/src/main/java/com/dearpet/dearpet/controller/ReviewController.java
@@ -22,31 +22,31 @@ public class ReviewController {
 
     // 상품 리뷰 목록 조회
     @GetMapping
-    public List<ReviewDTO> getAllReviewsByProductId(@RequestParam("product_id") Long productId) {
+    public List<ReviewDTO> getAllReviewsByProductId(@RequestParam("productId") Long productId) {
         return reviewService.getReviewsByProductId(productId);
     }
 
     // 리뷰 상세 정보 조회
-    @GetMapping("/{review_id}")
-    public ReviewDTO getReview(@PathVariable("review_id") Long reviewId) {
+    @GetMapping("/{reviewId}")
+    public ReviewDTO getReview(@PathVariable("reviewId") Long reviewId) {
         return reviewService.getReviewById(reviewId);
     }
 
     // 상품 리뷰 작성
-    @PostMapping("/products/{product_id}/reviews")
-    public ReviewDTO createReview(@RequestParam Long userId, @PathVariable("product_id") Long productId, @RequestBody ReviewDTO reviewDTO) {
+    @PostMapping("/products/{productId}/reviews")
+    public ReviewDTO createReview(@RequestParam Long userId, @PathVariable("productId") Long productId, @RequestBody ReviewDTO reviewDTO) {
         return reviewService.createReview(userId, productId, reviewDTO);
     }
 
     // 리뷰 수정
-    @PatchMapping("/{review_id}")
-    public ReviewDTO updateReview(@PathVariable("review_id") Long reviewId, @RequestBody ReviewDTO reviewDTO) {
+    @PatchMapping("/{reviewId}")
+    public ReviewDTO updateReview(@PathVariable("reviewId") Long reviewId, @RequestBody ReviewDTO reviewDTO) {
         return reviewService.updateReview(reviewId, reviewDTO);
     }
 
     // 리뷰 삭제
-    @DeleteMapping("/{review_id}")
-    public void deleteReview(@PathVariable("review_id") Long reviewId) {
+    @DeleteMapping("/{reviewId}")
+    public void deleteReview(@PathVariable("reviewId") Long reviewId) {
         reviewService.deleteReview(reviewId);
     }
 }
